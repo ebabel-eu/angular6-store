cfba8fb7c2dcac44afe6ca5282e76a7a8d88f5fb
Thomas Amar <hello@ebabel.eu>
2020-04-18 03:53:58 +0200

    Update app name.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular6Store
+# Tour of Heroes
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular6-store'`, () => {
+  it(`should have as title 'tour-of-heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular6-store');
+    expect(app.title).toEqual('tour-of-heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular6-store app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tour-of-heroes app is running!');
   });
 });
